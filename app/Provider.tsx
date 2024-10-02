b4fa52cecb6bda7287a0bf935ce87f7b4f714d84
dhanush-2313 <dhanusu006@gmail.com>
2024-10-02 18:52:24 +0530

    addedsentry

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -5,15 +5,26 @@ import {
   LiveblocksProvider,
 } from "@liveblocks/react/suspense";
 import { getClerkUsers } from "@/lib/actions/user.actions";
+import { getDocumentUsers } from "@/lib/actions/user.actions";
 import { ReactNode } from "react";
+import { useUser } from "@clerk/clerk-react";
 
 const Provider = ({ children }: { children: ReactNode }) => {
+  const { user: clerkUser } = useUser();
   return (
     <LiveblocksProvider
       authEndpoint="/api/liveblocks-auth"
       resolveUsers={async ({ userIds }) => {
         const users = await getClerkUsers({ userIds });
         return users;
+      }}
+      resolveMentionSuggestions={async ({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text,
+        });
+        return roomUsers;
       }}
     >
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>

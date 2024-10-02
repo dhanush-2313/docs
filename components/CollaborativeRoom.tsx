dd5fcd4d5aebfe9ac0c13f0125bc93d17b8fe9a6
dhanush-2313 <dhanusu006@gmail.com>
2024-10-02 17:19:06 +0530

    dynamic title done

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -5,18 +5,62 @@ import { Input } from "./ui/input";
 import Header from "@/components/Header";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import ActiveCollaborators from "./ActiveCollaborators";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import { updateDocument } from "@/lib/actions/room.actions";
 
 const CollaborativeRoom = ({
   roomId,
   roomMetadata,
 }: CollaborativeRoomProps) => {
+  const currentUserType = "editor";
   const [editing, setEditing] = useState(false);
   const [loading, setLoading] = useState(false);
-  const [doctitle, setDocTitle] = useState(roomMetadata.title);
+  const [docTitle, setDocTitle] = useState(roomMetadata.title);
 
   const containerRef = useRef<HTMLDivElement>(null);
-  const inputRef = useRef<HTMLDivElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const updateTitleHandler = async (
+    e: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (e.key === "Enter") {
+      setLoading(true);
+      try {
+        if (docTitle !== roomMetadata.title) {
+          const updatedDocument = await updateDocument(roomId, docTitle);
+          if (updatedDocument) {
+            setEditing(false);
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(e.target as Node)
+      ) {
+        setEditing(false);
+        updateDocument(roomId, docTitle);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [roomId, docTitle]);
+
+  useEffect(() => {
+    if (editing && inputRef.current) {
+      inputRef.current?.focus();
+    }
+  }, [editing]);
 
   return (
     <RoomProvider id={roomId}>
@@ -28,12 +72,37 @@ const CollaborativeRoom = ({
               className="flex w-fit items-center justify-center gap-2"
             >
               {editing && !loading ? (
-                <Input />
+                <Input
+                  type="text"
+                  value={docTitle}
+                  ref={inputRef}
+                  placeholder="Enter title"
+                  onChange={(e) => setDocTitle(e.target.value)}
+                  onKeyDown={updateTitleHandler}
+                  disabled={!editing}
+                  className="document-title-input"
+                />
               ) : (
                 <>
-                  <p className="document-title">{doctitle}</p>
+                  <p className="document-title">{docTitle}</p>
                 </>
               )}
+              {currentUserType === "editor" && !editing && (
+                <Image
+                  src="/assets/icons/edit.svg"
+                  alt="edit"
+                  width={24}
+                  height={24}
+                  onClick={() => setEditing(true)}
+                  className="pointer"
+                />
+              )}
+
+              {currentUserType !== "editor" && !editing && (
+                <p className="view-only-tag">View only</p>
+              )}
+
+              {loading && <p className="text-sm text-gray-400">saving...</p>}
             </div>
             <div className="flex w-full flex-1 justify-end gap-2 sm:gap-1">
               <ActiveCollaborators />
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -53,3 +53,18 @@ export const getDocument = async ({
     console.log(`Error getting a room: ${error}`);
   }
 };
+
+export const updateDocument = async (roomId: string, title: string) => {
+  try {
+    const updatedRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title,
+      },
+    });
+
+    revalidatePath(`/documents/${roomId}`);
+    return parseStringify(updatedRoom);
+  } catch (error) {
+    console.log(`Error updating title: ${error}`);
+  }
+};

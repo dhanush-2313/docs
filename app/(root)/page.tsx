92b2395888ccaf78ce1705ba52153e847a93b5f4
dhanush-2313 <dhanusu006@gmail.com>
2024-10-02 11:13:12 +0530

    slight changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,19 @@
+import Header from "@/components/Header";
 import { Button } from "@/components/ui/button";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notification
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+    </main>
   );
 }
